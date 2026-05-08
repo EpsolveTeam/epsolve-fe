91b9506cf95bc990780f3c6a640ef5e24f73d495
Dzvelocity <dzikrimurtadlo@gmail.com>
2026-05-08 13:43:50 +0700

    switch to explicit proxy route

--- api/proxy.js
+++ api/proxy.js
@@ -2,13 +2,9 @@ export default async function handler(req, res) {
   try {
     const baseURL = process.env.BACKEND_URL;
 
-    const rawPath = req.query.path;
+    const path = req.url.replace("/api/proxy", "");
 
-    const path = Array.isArray(rawPath)
-      ? rawPath.join("/")
-      : rawPath || "";
-
-    const url = `${baseURL}/${path}`;
+    const url = `${baseURL}${path}`;
 
     const response = await fetch(url, {
       method: req.method,
